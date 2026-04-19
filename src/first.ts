6548bdc3e14a6e57cdb2c5bf8ce736ca06848024
Jagrit Choudhary <jagritchoudhary@Jagrits-MacBook-Air.local>
2026-04-19 21:00:17 +0530

    Day-1

---
+++ src/first.ts
@@ -0,0 +1,4 @@
+function Greet (name :string) :string{
+    return `Hello ${name}`;
+}
+console.log(Greet("Jayesh"));
---
+++ src/typesinTS.ts
@@ -0,0 +1,16 @@
+//type inferencing 
+
+let drink = "cola"
+
+let perfumes = Math.random()>0.5 ? 10 :"5" //number|string
+
+let channelname = "Jayzz"
+
+// channelname = 1234 type error
+
+//type annotation 
+let chocolate :string = "Silk";
+chocolate = "Crispello";
+// chocolate = 2  u cant
+
+
